de6d5a8711afc8f7869ac9e4e396b95e170e1b44
David P. Chassin <david.chassin@me.com>
2022-02-08 06:23:05 -0800

    Update autotest.sh

--- autotest.sh
+++ autotest.sh
@@ -25,8 +25,8 @@ echo "Tests completed"
 echo "$TESTED tests completed"
 echo "$FAILED tests failed"
 if [ $FAILED -gt 0 ]; then
-    tar cfz validate-result.tar.gz $FILES
-    echo "Failure artifacts saved to validate-result.tar.gz"
+    tar cfz autotest-errors.tar.gz $FILES
+    echo "Failure artifacts saved to autotest-errors.tar.gz"
     exit 1
 else
     echo "Success"
